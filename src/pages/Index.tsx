93df42bf06290d966fbecbf91459e9cb9d74a758
LesterNevado <152840417+LesterNevado@users.noreply.github.com>
2025-02-23 22:29:41 +0800

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -57,7 +57,7 @@ export default function Index() {
       const { data, error } = await supabase.auth.signInWithOAuth({
         provider: 'google',
         options: {
-          redirectTo: `${window.location.origin}/dashboard`,
+          redirectTo: `${window.location.origin}/`,
           queryParams: {
             hd: 'neu.edu.ph',
             prompt: 'select_account',
