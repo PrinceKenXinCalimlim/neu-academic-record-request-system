6e81cf26c9d02b51ebf6c611f758c36b10305631
LesterNevado <152840417+LesterNevado@users.noreply.github.com>
2025-02-23 21:29:02 +0800

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -58,7 +58,9 @@ export default function Index() {
       const { data, error } = await supabase.auth.signInWithOAuth({
         provider: 'google',
         options: {
-          redirectTo: `${window.location.origin}/auth/callback`,
+          redirectTo: window.location.origin,
+
+          scopes: 'email profile'
           queryParams: {
             access_type: 'offline',
             prompt: 'consent',
